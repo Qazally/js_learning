ff9f32b23e875e351ceeb12171b116a504c10399
Qazllay <22014156-039@uog.edu.pk>
2025-06-15 15:10:18 +0500

    datatypes 2

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,21 @@
 "use strict"; //use all js code in new version
 //alert(2+2); //alert is used to show message not browser ,here we use node js
+
+let name = "qazal";
+let age = 20;
+let isloggeedIn = true;
+
+//number => 2 power 53 range
+//bigint => gereter than upper range
+//bollean => true/false
+//string =>c "qazal"
+//null => empty value/standalone value (null is a object,whenn we take its typeof it gives output "object")
+//undefined => not defined
+//symbol => unique value
+
+//object
+
+console.log(typeof "qazal");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
