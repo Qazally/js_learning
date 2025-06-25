09f5a2f9c2d611205bcbf4e7a1ddf76e3300bda2
Qazllay <22014156-039@uog.edu.pk>
2025-06-25 10:16:50 +0500

    datatypes_conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33;   //it is number
+let scores = "33"; // it is string
+
+console.log(typeof score);
+
+//console.log(typeof (score)); // same as above
+
+console.log(typeof scores);
+
+let valueInNumber = Number(scores);
+console.log(typeof valueInNumber);
+
+//"33" => 33
+//33abc => NaN (not a number)
+// true => 1 ;  / false => 0;
+
+let isLoggedIN = "1"; //if string is empty it will show false like
+let bolleanIsLoggedIn = Boolean(isLoggedIN);
+console.log(bolleanIsLoggedIn);
+
+//1 => true; / 0 => false
+// "" => false (empty striing)
+// "hitesh" => true 
+
