af3b71ef7a97356d40e55e767acef2e22b57c6fb
Qazllay <22014156-039@uog.edu.pk>
2025-07-08 16:21:31 +0500

    datatype_conversion

---
+++ 04_conversion.js
@@ -0,0 +1,8 @@
+console.log(2>1);//true
+
+console.log("2">1); //automatically convert it into number and then compare it ..//trure
+
+console.log(null>0); //false
+
+console.log(undefined==0);//false
+
